630676e6be2379a9e074b982ab8963d5184087b8
nrosed <nrd44@cornell.edu>
2022-01-28 18:23:22 -0700

    updated legend colors

--- utils/plotting_utils.R
+++ utils/plotting_utils.R
@@ -37,7 +37,7 @@ QUOTE_ANALYSIS_ORDER = c("nature_first",
 
 QUOTE_ANALYSIS_COLOR = c("#8DD3C7", "#1D4942", "#FFFFB3", "#98c700", "#BC80BD", 
                         "#FB8072", "#BEBADA", "#94e283", "#FFED6F", "#FCCDE5", 
-                        "#FB8072", "#FCCDE5", "#7FA0BD", "#7FA0BD")#, "3D5B76")
+                        "#FB8072", "#FCCDE5", "#7FA0BD", "#3D5B76")#, "3D5B76")
 names(QUOTE_ANALYSIS_COLOR) = QUOTE_ANALYSIS_ORDER
 
 QUOTE_ANALYSIS_LABELS = c("Nature First",
